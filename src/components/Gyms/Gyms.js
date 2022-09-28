2fa3caa08fbcd48cc3f678af62809e42afa3a98e
Hasanul Bari <hasanulbari8@gmail.com>
2022-09-28 23:05:26 +0600

    data load complete

--- src/components/Gyms/Gyms.js
+++ src/components/Gyms/Gyms.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Gyms = () => {
+    const [gyms, setGyms] = useState([]);
+    useEffect(() => {
+        fetch('gym.json')
+            .then(res => res.json())
+        .then(data =>setGyms(data))
+    }, [])
+    console.log(gyms)
     return (
         <div>
             <h1>I am fropm gyms</h1>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Gyms from '../Gyms/Gyms';
 
 const Home = () => {
     return (
         <div className='grid grid-cols-4'>
             <div className='col-span-3'>
-                <h1>hsan</h1>
+                <Gyms></Gyms>
             </div>
             <div className='bg-blue-600 ml-2'>
                  <h1>Add to cart</h1>
