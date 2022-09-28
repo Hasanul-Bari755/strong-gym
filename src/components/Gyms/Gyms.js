9679a6a80d98a019003a7e943ecdb0501b1bd946
Hasanul Bari <hasanulbari8@gmail.com>
2022-09-28 22:59:12 +0600

    setup complete

---
+++ src/components/Gyms/Gyms.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gyms = () => {
+    return (
+        <div>
+            <h1>I am fropm gyms</h1>
+        </div>
+    );
+};
+
+export default Gyms;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='grid grid-cols-4'>
+            <div className='col-span-3'>
+                <h1>hsan</h1>
+            </div>
+            <div className='bg-blue-600 ml-2'>
+                 <h1>Add to cart</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
